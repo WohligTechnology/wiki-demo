5cb9c4d51821cdd51dc634d361b5bbe1d65fdb4f
sohan1 <shn619@gmail.com>
2018-02-15 10:12:32 +0530

    marker changes

---
+++ platforms/android/app/src/main/assets/www/world/09_ObtainPoiData_3_FromWebservice/json/GetSamplePois.js
@@ -0,0 +1,71 @@
+[{
+        "id": "1",
+        "longitude": "0.021",
+        "latitude": "0.006",
+        "description": "This is the description of POI#1",
+        "name": "POI#1"
+    },
+    {
+        "id": "2",
+        "longitude": "-0.09",
+        "latitude": "-0.086",
+        "description": "This is the description of POI#2",
+        "name": "POI#2"
+    },
+    {
+        "id": "3",
+        "longitude": "-0.008",
+        "latitude": "-0.082",
+        "description": "This is the description of POI#3",
+        "name": "POI#3"
+    },
+    {
+        "id": "4",
+        "longitude": "0.055",
+        "latitude": "-0.067",
+        "description": "This is the description of POI#4",
+        "name": "POI#4"
+    },
+    {
+        "id": "5",
+        "longitude": "-0.072",
+        "latitude": "0.07",
+        "description": "This is the description of POI#5",
+        "name": "POI#5"
+    },
+    {
+        "id": "6",
+        "longitude": "0.02",
+        "latitude": "-0.045",
+        "description": "This is the description of POI#6",
+        "name": "POI#6"
+    },
+    {
+        "id": "7",
+        "longitude": "0.017",
+        "latitude": "-0.056",
+        "description": "This is the description of POI#7",
+        "name": "POI#7"
+    },
+    {
+        "id": "8",
+        "longitude": "0.077",
+        "latitude": "0.089",
+        "description": "This is the description of POI#8",
+        "name": "POI#8"
+    },
+    {
+        "id": "9",
+        "longitude": "-0.02",
+        "latitude": "-0.016",
+        "description": "This is the description of POI#9",
+        "name": "POI#9"
+    },
+    {
+        "id": "10",
+        "longitude": "0.084",
+        "latitude": "-0.035",
+        "description": "This is the description of POI#10",
+        "name": "POI#10"
+    }
+]
